a10d625cdde9f9a54cbd55783143cd9d9eda9d93
Mr.Innocent <100468303+ShinanSky@users.noreply.github.com>
2025-07-25 20:42:28 +0800

    Update Live2D.js

--- components/Live2D.js
+++ components/Live2D.js
@@ -47,8 +47,8 @@ export default function Live2D() {
   return (
     <canvas
       id='live2d'
-      width='280'
-      height='1400'
+      width='290'
+      height='1100'
       onClick={handleClick}
       className='cursor-grab'
       onMouseDown={e => e.target.classList.add('cursor-grabbing')}
