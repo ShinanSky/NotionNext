73ab64659d2a0a53fb605e36c69a95629355a9b2
Mr.Innocent <100468303+ShinanSky@users.noreply.github.com>
2025-07-29 00:15:11 +0800

    Update Footer.js

--- themes/gitbook/components/Footer.js
+++ themes/gitbook/components/Footer.js
@@ -19,7 +19,7 @@ const Footer = ({ siteInfo }) => {
 
       <div className='flex justify-center'>
         <div>
-          <i className='mx-1 animate-pulse fas fa-pen' />{' '}
+          <i className='mx-1 animate-pulse fa-solid fa-pen' />{' '}
           <a
             href={siteConfig('LINK')}
             className='underline font-bold text-gray-500 dark:text-gray-300 '>
